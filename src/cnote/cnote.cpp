317ade04df5d38177c10d331ec27c21fd365b8ee
ambiesoft <ambiesoft.trueff@gmail.com>
2021-06-01 17:50:20 +0900

    verup

--- src/cnote/cnote.cpp
+++ src/cnote/cnote.cpp
@@ -14,7 +14,7 @@ using namespace Ambiesoft;
 using namespace Ambiesoft::stdosd;
 
 #define CNOTE_APPNAME L"cnote"
-#define CNOTE_VERSION L"1.0.5"
+#define CNOTE_VERSION L"1.0.6"
 
 void ShowErrorAndExit(const wstring& message)
 {
