daa682a3ab4b0cd3da80216f6fb95572fb349326
ambiesoft <ambiesoft.trueff@gmail.com>
2022-03-29 05:33:40 +0900

    Added 'txvr' for viewer Added option '--get-default-viewer', '--set-default-viewer' and '--viewer'

--- src/cnote/cnote.cpp
+++ src/cnote/cnote.cpp
@@ -9,15 +9,21 @@
 #include "../../../lsMisc/UTF16toUTF8.h"
 #include "../../../lsMisc/stdosd/stdosd.h"
 #include "../../../lsMisc/IsWindowsVersion.h"
+#include "../../../lsMisc/CommandLineParser.h"
+
+#include "../../../profile/cpp/Profile/include/ambiesoft.profile.h"
 
 #include "detect.h"
+
+#define SECTION_OPTION "Option"
+#define KEY_DEFAULT_VIEWER "DefaultViewer"
 
 using namespace std;
 using namespace Ambiesoft;
 using namespace Ambiesoft::stdosd;
 
 #define CNOTE_APPNAME L"cnote"
-#define CNOTE_VERSION L"1.0.9"
+#define CNOTE_VERSION L"1.0.10"
 
 void ShowErrorAndExit(const wstring& message)
 {
@@ -29,22 +35,28 @@ void ShowVersionAndExit()
 	wcout << CNOTE_APPNAME << L" v" << CNOTE_VERSION << endl;
 	exit(0);
 }
-void ShowHelpAndExit()
+//void ShowHelpAndExit()
+//{
+//	wcout << 
+//R"(cnote can be used as if 'more' or 'less'.
+//
+//ex:
+//    dir | cnote
+//
+//options:
+//	-W : Convert to Windows-styled newline
+//	-V -v --version: Shows version
+//	-h --help : Shows help
+//)" << endl;
+//	exit(0);
+//}
+
+wstring GetIniFile()
 {
-	wcout << 
-R"(cnote can be used as if 'more' or 'less'.
-
-ex:
-    dir | cnote
-
-options:
-	-W : Convert to Windows-styled newline
-	-V -v --version: Shows version
-	-h --help : Shows help
-)" << endl;
-	exit(0);
+	return stdCombinePath(
+		stdGetParentDirectory(stdGetModuleFileName()),
+		stdGetFileNameWitoutExtension(stdGetModuleFileName()) + L".ini");
 }
-
 #define RETRUN_WITH_ERROR(ERRROSTRING) do {			\
 	DWORD dwLE = GetLastError();					\
 	ShowErrorAndExit(I18N(ERRROSTRING) +			\
@@ -55,29 +67,109 @@ int wmain(int argc, const wchar_t* argv[])
 {
 	bool bCRLF = false;
 	bool bVerbose = false;
+	bool bVersion = false;
+	bool bHelp = false;
+	bool bGetDefaultViewer = false;
+	wstring defaultViewerToSet;
+	wstring viewer;
+
 	const bool bIsWin11 = IsWindows11OrAbove();
 
-	for (int i = 1; i < argc; ++i)
+	CCommandLineParser parser(I18N(L"Views standard outputs in a gui viewer"), CNOTE_APPNAME);
+
+	parser.AddOption(L"-W",
+		ArgCount::ArgCount_Zero,
+		&bCRLF,
+		ArgEncodingFlags_Default,
+		I18N(L"Convert to Windows-styled newline"));
+	parser.AddOption(L"--verbose",
+		ArgCount::ArgCount_Zero,
+		&bVerbose,
+		ArgEncodingFlags_Default,
+		I18N(L"Shows verbose outputs"));
+	parser.AddOptionRange({ L"-v",L"-V",L"--version" },
+		ArgCount::ArgCount_Zero,
+		&bVersion,
+		ArgEncodingFlags_Default,
+		I18N(L"Shows version"));
+	parser.AddOptionRange({ L"-h",L"--help",L"/?",L"/h" },
+		ArgCount::ArgCount_Zero,
+		&bHelp,
+		ArgEncodingFlags_Default,
+		I18N(L"Shows Help"));
+	parser.AddOption(L"--get-default-viewer",
+		ArgCount::ArgCount_Zero,
+		&bGetDefaultViewer,
+		ArgEncodingFlags_Default,
+		I18N(L"Shows default viewer"));
+	parser.AddOption(L"--set-default-viewer",
+		ArgCount::ArgCount_One,
+		&defaultViewerToSet,
+		ArgEncodingFlags_Default,
+		I18N(L"Sets default viewer, one of 'notepad' or 'txvr'"));
+	parser.AddOption(L"--viewer",
+		ArgCount::ArgCount_One,
+		&viewer,
+		ArgEncodingFlags_Default,
+		I18N(L"Viewer to use"));
+
+	parser.Parse();
+
+	if (parser.hadUnknownOption())
 	{
-		if(false){}
-		else if (wcscmp(argv[i], L"-W") == 0)
-			bCRLF = true;
-		else if (wcscmp(argv[i], L"-v") == 0 || wcscmp(argv[i], L"-V") == 0 || wcscmp(argv[i], L"--version") == 0)
+		ShowErrorAndExit(stdFormat(L"Unknown option '%s'", parser.getUnknowOptionStrings().c_str()));
+	}
+	if (bVersion)
+	{
+		ShowVersionAndExit();
+	}
+	if (bHelp)
+	{
+		wcout << parser.getHelpMessage() << endl;
+		return 0;
+	}
+	if (bGetDefaultViewer)
+	{
+		string dv;
+		Profile::GetString(SECTION_OPTION, KEY_DEFAULT_VIEWER, "", dv, GetIniFile());
+		if (dv.empty())
+			dv = "notepad";
+		cout << dv << endl;
+		return 0;
+	}
+	if (!defaultViewerToSet.empty())
+	{
+		if (defaultViewerToSet != L"notepad" && defaultViewerToSet != L"txvr")
 		{
-			ShowVersionAndExit();
+			wcerr << I18N(L"Default viewer must be one of 'notepad' or 'txvr'.") << endl;
+			return 1;
 		}
-		else if (wcscmp(argv[i], L"-h") == 0 || wcscmp(argv[i], L"--help") == 0)
+		bool success = true;
+		success &= Profile::WriteString(SECTION_OPTION, KEY_DEFAULT_VIEWER, 
+			toStdUtf8String(defaultViewerToSet), GetIniFile());
+		if (!success)
 		{
-			ShowHelpAndExit();
+			wcerr << I18N(L"Failed to save ini file.") << endl;
+			return 1;
 		}
-		else if (wcscmp(argv[i], L"--verbose")==0)
-		{
-			bVerbose = true;
-		}
-		else
-		{
-			ShowErrorAndExit(stdFormat(L"Unknown option '%s'", argv[i]));
-		}
+		return 0;
+	}
+	if (viewer.empty())
+	{
+		string v;
+		Profile::GetString(SECTION_OPTION, KEY_DEFAULT_VIEWER, "", v, GetIniFile());
+		if (v.empty())
+			v = "notepad";
+		viewer = toStdWstringFromUtf8(v);
+	}
+	wstring viewerArg;
+	if (viewer == L"txvr")
+	{
+		viewer = stdCombinePath(
+			stdGetParentDirectory(stdGetModuleFileName()),
+			viewer);
+		viewerArg = L"--blank";
+		bCRLF = true;
 	}
 
 	vector<char> all;
@@ -105,20 +197,20 @@ int wmain(int argc, const wchar_t* argv[])
 
     // how to handle window handle in linux?
     CKernelHandle process;
-	if (!OpenCommon(nullptr, L"notepad", nullptr, nullptr, &process))
+	if (!OpenCommon(nullptr, viewer.c_str(), viewerArg.c_str(), nullptr, &process))
 	{
-		RETRUN_WITH_ERROR(L"Failed to open notepad");
+		RETRUN_WITH_ERROR(stdFormat(I18N(L"Failed to open '%s'"), viewer.c_str()).c_str());
 	}
 
 	if (WAIT_OBJECT_0 != WaitForInputIdle(process, INFINITE))
 	{
-		RETRUN_WITH_ERROR(L"Failed to wait notepad");
+		RETRUN_WITH_ERROR(stdFormat(I18N(L"Failed to wait '%s'"),viewer.c_str()).c_str());
 	}
 
 	auto allTops = FindTopWindowFromPID(GetProcessId(process));
 	if (allTops.empty())
 	{
-		RETRUN_WITH_ERROR(L"Failed to find notepad");
+		RETRUN_WITH_ERROR(stdFormat(I18N(L"Failed to find '%s'"), viewer.c_str()).c_str());
 	}
 
 	HWND hEditNotepad = NULL;
